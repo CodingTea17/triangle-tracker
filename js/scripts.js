1713b80b0981772fc7a83260772d2bde3e9a2a6b
Dawson Mortenson and Javier Jimenez <dawson@>
2017-08-10 16:01:03 -0700

    add backend functions to the scripts.js file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,53 @@
-$(document).ready(function() {
- $("#form-triangleTracker").submit(function(event) {
-   event.preventDefault();
-   var sideLeft = parseInt($("#sideLeft").val());
-   var sideRight = parseInt($("#sideRight").val());
-   var bottom = parseInt($("#sideBottom").val());
+// BACKEND
+function isEquilateral(side1, side2, side3){
+  if(side1 === side2 && side2 === side3){
+    return true;
+  } else {
+    return false;
+  }
+}
 
-   if ((bottom + sideRight) <= sideLeft) {
+function notATriangle(side1, side2, side3){
+  if((side3 + side2) <= side1){
+    return true;
+  } else if ((side3 + side1) <= side1) {
+    return true;
+  } else if ((side1 + side2) <= side3) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isIsosceles(side1, side2, side3){
+  if (side1 === side2) {
+    return true;
+  } else if (side1 === side3) {
+    return true;
+  } else if (side3 === side2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// FRONTEND
+$(document).ready(function() {
+  $("#form-triangleTracker").submit(function(event) {
+    event.preventDefault();
+    var sideLeft = parseInt($("#sideLeft").val());
+    var sideRight = parseInt($("#sideRight").val());
+    var bottom = parseInt($("#sideBottom").val());
+
+    if (notATriangle(sideLeft, sideRight, bottom)){
       alert("Not a triangle");
-   } else if ((bottom + sideLeft) <= sideLeft) {
-      alert("Not a triangle");
-   } else if ((sideLeft + sideRight) <= bottom) {
-      alert("Not a triangle");
-   } else if(sideLeft === sideRight && sideRight === bottom){
-     alert("Equilateral");
-   } else if (sideLeft === sideRight) {
-      alert("2sides are good");
-   } else if (sideLeft === bottom) {
-      alert("2sides are good");
-   } else if (bottom === sideRight) {
-      alert("2sides are good");
-   } else {
-     // THE PART THAT PUTS TEXT INTO THE "whatTriangle" ID
-     $("#whatTriangle").text("Scalene");
-   }
-   $("#whatTriangle").show();
- });
+    } else if (isEquilateral(sideLeft, sideRight, bottom)){
+      alert("Equilateral");
+    } else if (isIsosceles(sideLeft, sideRight, bottom)){
+      alert("Isosceles");
+    } else {
+      // THE PART THAT PUTS TEXT INTO THE "whatTriangle" ID
+      $("#whatTriangle").text("Scalene");
+    }
+    $("#whatTriangle").show();
+  });
 });
